3f8c534190009432b6fbe54b4fa9ab77215cacf4
Surajk37 <surajkhot9993@gmail.com>
2023-01-07 16:49:59 +0530

    Welcome message Uploaded

---
+++ LinkedList/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Linked List [Data Structure] Programs");
+            Console.ReadLine();
+        }
+    }
+}
