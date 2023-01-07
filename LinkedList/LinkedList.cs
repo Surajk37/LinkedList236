4af2f06e3847c6329528b07d80e89a4f6552b7cb
Surajk37 <surajkhot9993@gmail.com>
2023-01-07 21:07:31 +0530

    To create Linked List by adding 30 and 56 to 70

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -6,32 +6,52 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class LinkedList
+    public class LinkedList
     {
         //Instance
-        internal Node head;
+        public Node head;
 
         //Adding Data to LinkedList
-        internal void Add(int data)
+        public void Add(int data)
         {
             //Object of Node
             Node node = new Node(data);
 
             //Add Data
             if (this.head == null)
+            { 
                 this.head = node;
+                Console.WriteLine("{0}: Inserted SuccessFully ", node.data);
+            }
+            else
+            {
+                Node addr = new Node(data);
+                Node h1 = null;
+                h1 = this.head;
+                this.head = addr;
+                Node temp = addr;
+                temp.next = h1;
+                Console.WriteLine(addr.data + ": Inserted SuccessFully");
+            }
+
+            //Displaying  LinkedList Data
+        }
+        public void Display()
+        {
+            //Head is Null then List is empty
+            if (this.head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
             else
             {
                 Node temp = head;
-                while (temp.next != null)
+                while (temp != null)
                 {
+                    Console.WriteLine("Linked List Data is : " + temp.data);
                     temp = temp.next;
                 }
-                temp.next = node;
             }
-
-            //Display LinkedList Data
-            Console.WriteLine("{0} inserted into the Linked List", node.data);
         }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,12 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\tWelcome To Linked List [Data Structure] Programs\n");
-
+             
             //Calling Class LinkedList
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
+            LinkedList linkedList = new LinkedList(); 
             linkedList.Add(70);
+            linkedList.Add(30);
+            linkedList.Add(56);
+            linkedList.Display();
+
             Console.ReadLine();
         }
     }
