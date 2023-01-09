f87f5b7315f84c3db544d64261dd9e11e9a0ef4e
Surajk37 <surajkhot9993@gmail.com>
2023-01-09 16:52:19 +0530

    Ability to delete the first element in the LinkedList

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -59,6 +59,14 @@ namespace LinkedList
                 temp.next = node;
             }
         }
+        //Delete First element 
+       public Node POP()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
         //Displaying  LinkedList Data
         public void Display()
         {
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,19 +5,43 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\tWelcome To Linked List [Data Structure] Programs\n");
-             
-            //Calling Class LinkedList
-            LinkedList linkedList = new LinkedList(); 
-           
-            linkedList.Add(56);
-            linkedList.Add(70);
-            Console.WriteLine("\n\tBefore Inserting new node to Linked List");
-            linkedList.Display();
-            linkedList.InsertAtParticularPosition(2, 30);
-            Console.WriteLine("\n\n\tAfter Inserting new node to Linked List");
-            linkedList.Display();
-
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Please Select option from below list");
+                Console.WriteLine("\n1. Add Linked List \n2. Insert New Node \n3. Delete First Node");
+                int option = Convert.ToInt32(Console.ReadLine());
+                //Calling Class LinkedList
+                LinkedList linkedList = new LinkedList();
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        linkedList.Add(56);
+                        linkedList.Add(70);
+                        Console.WriteLine("\n\tBefore Inserting new node to Linked List");
+                        linkedList.Display();
+                        linkedList.InsertAtParticularPosition(2, 30);  //inserting new node to linked list 
+                        Console.WriteLine("\n\n\tAfter Inserting new node to Linked List");
+                        linkedList.Display();
+                        break;
+                    case 3:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        Console.WriteLine("\n\tBefore Deleting node from Linked List");
+                        linkedList.Display();
+                        linkedList.POP();    //to delete a first node from linked list
+                        Console.WriteLine("\n\n\tAfter Deleting First Node from  Linked List");
+                        linkedList.Display();
+                        break;
+                }
+                Console.ReadLine();
+            }           
         }
     }
 }
